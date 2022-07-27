f165e951322607460a080a2a185071f11e3a5941
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-07-27 14:59:20 -0500

    Buttons and started S/CSS file

---
+++ script.js
@@ -0,0 +1,7 @@
+/* each button should represent it's own number or character (*+/-)
+input should be inputted instead of console.log
+Button for each thing
+make super simple first then add styling
+must get javascript down first, then styling 
+there must be a disolay that shows the numbers being input
+AC button must clear the screen.*/
