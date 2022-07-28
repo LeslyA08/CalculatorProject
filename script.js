ea5afb68369aa59c933ac3c2a7214ee17a16d6de
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-07-28 10:27:19 -0500

    Displays numbers

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ must get javascript down first, then styling
 there must be a disolay that shows the numbers being input
 AC button must clear the screen.
 once button is pressed and appears on the screen, start formulations/functions*/
+
+const button = document.querySelectorAll(".button");
+const input = document.querySelector(".input1")
+const form = document.querySelector("form");
+
