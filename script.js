6e7c12cfb5225b24fcdfd330e10ac23eda985b8b
Lesly Alvarez <leslyalvarez08@gmail.com>
2022-08-03 14:45:07 -0500

    Redo display

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-/* each button should represent it's own number or character (*+/-)
-input should be inputted instead of console.log
-Button for each thing
-make super simple first then add styling
-must get javascript down first, then styling 
-there must be a disolay that shows the numbers being input
-AC button must clear the screen.
-once button is pressed and appears on the screen, start formulations/functions*/
-
-const calculate= (number1, operator, number2) => {
+/*const calculate= (number1, operator, number2) => {
   let result = ''
   if (operator === 'add') {
   result = number1 + number2;
@@ -19,10 +10,89 @@ else if (operator === 'multiply') {
   result = number1 * number2;
 }
 else if (operator === 'divide') {
-  result = (number1 / number2);
+  result =number1 / number2;
 }
 return result;
+} */
+
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.calculator_display')
+const operatorKeys = calculator.querySelectorAll('.keyOperator')
+const numKeys= calculator.querySelectorAll('.numKey')
+
+let number1 = ""
+let operatorKey = "" 
+let number2 = ""
+
+numKeys.forEach((number) => {
+  number.addEventListener('click', () => {
+    number1 += parseInt(number.innerHTML)
+    number2 += parseInt(number.innerHTML)
+    display.innerHTML += number.innerHTML;
+  })
+})
+
+operatorKeys.forEach((operator) => {
+  operator.addEventListener('click', () =>{
+    operatorKey = operator.innerHTML
+    display.innerHTML += operator.innerHTML;
+  })
+})
+
+const calculate = () =>{
+  return number1
 }
 
-keys.addEventListener('click'), e => {
-  if (e.target.matches('button')) }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*numKeys.addEventListener("click", e => {
+ if (e.target.matches("button")) {
+  const key = e.target
+  const action = key.dataset.action
+  const keyContent = key.textContent
+  const displayedNum = display.textContent
+
+  if (!action) {
+    if (displayedNum === '0') {
+      display.textContent += keyContent;
+    }else {
+      display.textContent = displayedNum + keyContent;
+    }
+  }
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    display.textContent = keyContent
+  }else {
+    display.textContent = displayedNum + keyContent
+  }
+  if (action === 'decimal') {
+    display.textContent = keyContent
+  }else {
+    display.textContent = displayedNum + keyContent
+  }
+  
+  if (action === 'clear') {
+  }
+  
+  if (action === 'calculate') {
+    
+  }
+
+ }
+});*/
+
